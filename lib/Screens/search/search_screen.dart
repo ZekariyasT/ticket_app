1101028eaf31988661353b995826d171044ae3e2
Zekariyas Tesfaye <99519232+ZekariyasT@users.noreply.github.com>
2024-08-09 11:10:14 +0300

    done

--- lib/Screens/search/search_screen.dart
+++ lib/Screens/search/search_screen.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:ticket_app/Screens/search/widgets/app_ticket_tabs.dart';
+import 'package:ticket_app/base/res/styles/app_styles.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppStyles.bgColor,
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        children: [
+          const SizedBox(height: 40),
+          Text(
+            "What are\nyou looking for ?",
+            style: AppStyles.headLineStyle1.copyWith(fontSize: 35),
+          ),
+          const SizedBox(height: 20),
+          const AppTicketTabs(),
+        ],
+      ),
+    );
   }
 }
